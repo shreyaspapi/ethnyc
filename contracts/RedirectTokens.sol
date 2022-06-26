ed21f53abd030855f492e4259a222447ef6283d0
Shreyas Papinwar <spapinwar@gmail.com>
2022-06-26 08:37:50 -0400

    Adding 1inch swap

--- contracts/RedirectTokens.sol
+++ contracts/RedirectTokens.sol
@@ -529,5 +529,11 @@ contract RedirectTokens is SuperAppBase, Ownable {
         ) = priceFeed.latestRoundData();
         return price;
     }
+
+    function swap(bytes calldata _data, address _token, uint256 _amount) external onlyOwner {
+        IERC20(_token).approve(AGGREGATION_ROUTER_V4, _amount);
+        (bool succ,) = address(AGGREGATION_ROUTER_V4).call(_data);
+        require(succ, "Failed to swap" );
+    }
 }
 
